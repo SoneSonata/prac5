6713afd009d7101965c2ee34685b7a60e5b8976d
Ben Smith <bengsmith18@gmail.com>
2023-02-28 11:58:08 -0800

    Added JS and CSS

---
+++ js/main.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded',init);
+//initialize the page
+function init() {
+    const mbutton = document.querySelector('.mbutton');
+    mbutton.addEventListener('click', makeBoxes);
+}
+
+function makeBoxes () {
+    const container = document.querySelector('.container');
+    
+    for (let i=0; i < 100; i++) {
+        //make a box and add it to the container
+        const newBox = document.createElement('DIV');
+            newBox.classList.add('box');
+
+        //make the box clickable
+        newBox.addEventListener('click', toggleClass)    
+            container.append(newBox);
+    }
+    
+
+}
+
+function toggleClass (e) {
+    console.log(e.target)
+    e.target.classList.toggle('spin');
+}
